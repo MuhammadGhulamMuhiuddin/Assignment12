08a4cc405f7c0280b1248e43c11c80e42808112a
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-10-06 19:50:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+// // var useri prompt("user1" "0 or 1")
+
+// // var user2 prompt("user2: "8 or 1")
+
+
+// // var num Math.floor(Math.random()*2)
+// // if (user1 || user2 == num){
+
+// //
+
+// if(user1== num){
+
+
+//     //
+    
+//     console.log(num" User1 Win")
+    
+    
+    
+//     }else{
+    
+    
+    
+//     //
+    
+//     console.log(num+" User2 Win")
+    
+//      //
+    
+    
+    
+//     //)
+    
+    
+    
+    
+    
+//     I
+    
+    
+//     // if (user == num) (
+    
+//     / console.log("num You Win");
+    
+//     // var num = Math.floor(Math.random()*7)
+    
+//     // var user = prompt("Select any no. from 1-6");
+    
+    
+//     //} else {
+    
+//     console.log("num Computer Win");
+    
+    
+//     //}
+    
+    
+    
+    
+//     var num Math.floor(Math.random()*7);
+    
+//     console.log ("Your dice Roll and apper+num");
